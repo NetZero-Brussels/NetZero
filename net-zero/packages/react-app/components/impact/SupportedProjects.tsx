898266cec7d6aa9a7c5751babb8a07865f2746cd
Fernando Rabasco Ledesma <frabascol@gmail.com>
2024-07-13 23:10:29 +0200

    List of Supported projects

--- net-zero/packages/react-app/components/impact/SupportedProjects.tsx
+++ net-zero/packages/react-app/components/impact/SupportedProjects.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import ProjectList from "./ProjectList";
 
 export function SupportedProjects() {
     return (
-        <div className="">
-
+        <div className="flex flex-col items-start gap-[8px] self-stretch">
+            <div className='text-[24px] not-italic font-medium leading-[32px] font-[Futura]'>
+                <h1>Supported projects</h1>
+            </div>
+            <div>
+                <p className='font-normal'>Check the Projects you have supported.</p>
+            </div>
+            <ProjectList />
         </div>
     );
 }
